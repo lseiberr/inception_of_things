c4e54711a2542c866a8b68db2fc2d29953e8de49
lseiberr <lucas.seib@icloud.com>
2025-12-04 17:57:09 +0100

    Mettre à jour les boîtes Vagrant vers ubuntu/jammy64 pour les serveurs et les travailleurs

--- p1/agent.sh
+++ p1/agent.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euxo pipefail
 
 # Mise à jour des paquets
 sudo apt-get update -y
@@ -9,5 +10,10 @@ K3S_URL=https://192.168.56.110:6443
 K3S_TOKEN="$1"  # le token sera passé en argument par Vagrant
 curl -sfL https://get.k3s.io | K3S_URL=$K3S_URL K3S_TOKEN=$K3S_TOKEN sh -
 
-# Installation de kubectl
+# Installation de kubectl (via dépôt officiel Kubernetes)
+sudo apt-get install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update -y
 sudo apt-get install -y kubectl
